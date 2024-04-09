a84d3382ebc2d71de1ecadc0f69188bcccf1f84f
Анатолій <t0663261690@gmail.com>
2024-04-09 16:17:31 +0300

    Update bookstore.sql
    finall_test

--- bookstore.sql
+++ bookstore.sql
@@ -1,57 +1,60 @@
-create database [bookstore]
+use [bookstore]
 go
 
 -- 1
 SELECT * FROM Books WHERE Pages > 500 AND Pages < 650;
 
 -- 2
-SELECT * FROM Books WHERE LEFT(Name, 1) IN ('A', 'H');
+SELECT * FROM Books WHERE LEFT(Name, 1) IN ('A', 'H'); 
 
 --3
-SELECT b.* 
-FROM Books b
-JOIN Sales s ON b.Id = s.BookId
-JOIN Themes t ON b.ThemeId = t.Id
-WHERE t.Name = 'Детектив' AND s.Quantity > 30;
+SELECT Books.* 
+FROM Books
+JOIN Sales ON Books.Id = Sales.BookId
+JOIN Themes ON Books.ThemeId = Themes.Id
+WHERE Themes.Name = 'Детектив' AND Sales.Quantity > 30;
 
 --4
 SELECT * FROM Books WHERE Name LIKE '%Microsoft%' AND Name NOT LIKE '%Windows%';
 
 --5
-SELECT CONCAT(b.Name, ', ', t.Name, ', ', a.Name, ' ', a.Surname) AS Book_Info
-FROM Books b
-JOIN Themes t ON b.ThemeId = t.Id
-JOIN Authors a ON b.AuthorId = a.Id
-WHERE b.Price / b.Pages < 0.65;
+SELECT CONCAT(Books.Name, ', ', Themes.Name, ', ', Authors.Name, ' ', Authors.Surname) AS Book_Themes_Authors
+FROM Books
+JOIN Themes ON Books.ThemeId = Themes.Id
+JOIN Authors ON Books.AuthorId = Authors.Id
+WHERE Books.Price / Books.Pages < 0.65;
 
 --6
 SELECT * FROM Books WHERE LEN(Name) - LEN(REPLACE(Name, ' ', '')) = 3;
 
---7 не працює
+--7 
 SELECT 
-    Books.Name AS Book_Name,
-    Themes.Name AS Theme,
-    CONCAT(Authors.Name, ' ', Authors.Surname) AS Author_Name,
-    Sales.Price AS Sale_Price,
-    Sales.Quantity AS Sale_Quantity,
-    Shops.Name AS Shop_Name
+    b.Name AS Book,
+    t.Name AS Theme,
+    CONCAT(a.Name, ' ', a.Surname) AS Author_Name,
+    s.Price AS Price,
+    s.Quantity AS Quantity,
+    sh.Name AS Shop
 FROM 
-    Sales
+    Sales s
 JOIN 
-    Books ON Sales.BookId = Books.Id
+    Books b ON s.BookId = b.Id
 JOIN 
-    Themes ON Books.ThemeId = Themes.Id
+    Authors a ON b.AuthorId = a.Id
 JOIN 
-    Authors ON Books.AuthorId = Authors.Id
+    Themes t ON b.ThemeId = t.Id
 JOIN 
-    Shops ON Sales.ShopId = Shops.Id
+    Shops sh ON s.ShopId = sh.Id
+JOIN 
+    Countries c ON sh.CountryId = c.Id
 WHERE 
-    Books.Name NOT LIKE '%A%' 
-    AND Themes.Name != 'Програмування' 
-    AND CONCAT(Authors.Name, ' ', Authors.Surname) != 'Герберт Шилдт' 
-    AND Sales.Price BETWEEN 10 AND 20 
-    AND Sales.Quantity >= 30 
-    AND Shops.CountryId NOT IN (1, 2); 
+    b.Name NOT LIKE '%A%' 
+    AND t.Name != 'Програмування' 
+    AND CONCAT(a.Name, ' ', a.Surname) != 'Герберт Шилдт' 
+    AND s.Price BETWEEN 10 AND 20 
+    AND s.Quantity >= 8 
+    AND c.Name NOT IN ('Spain', 'Italy');
+
 
 -- 8
 SELECT 'Кількість авторів' AS Description, COUNT(*) AS Value FROM Authors
@@ -65,7 +68,7 @@ SELECT 'Середня кількість сторінок' AS Description, AVG(
 --9
 SELECT 
     Themes.Name AS Theme,
-    SUM(Books.Pages) AS Total_Pages
+    SUM(Books.Pages) AS Sum_Pages
 FROM 
     Books
 JOIN 
@@ -75,9 +78,9 @@ GROUP BY
 
 --10
 SELECT 
-    CONCAT(Authors.Name, ' ', Authors.Surname) AS Author_Name,
-    COUNT(Books.Id) AS Total_Books,
-    SUM(Books.Pages) AS Total_Pages
+    CONCAT(Authors.Name, ' ', Authors.Surname) AS Author,
+    COUNT(Books.Id) AS Books,
+    SUM(Books.Pages) AS Sum_Pages
 FROM 
     Books
 JOIN 
@@ -128,10 +131,10 @@ GROUP BY
 SELECT 
     Books.Name AS Book,
     Authors.Name AS Author,
-    Sales.Quantity AS Sold_Quantity,
+    Sales.Quantity AS Quantity,
     Shops.Name AS Shops,
     Countries.Name AS [Location],
-    Sales.SaleDate AS Sale_Date
+    Sales.SaleDate AS Date_Sales
 FROM 
     Sales
 JOIN 
